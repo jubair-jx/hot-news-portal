9fbee136d101256fae3067294a277c5617fb56bc
Mohammad Jubair <you@example.com>
2023-03-02 20:23:41 +0600

    Final Push

--- scripts/news.js
+++ scripts/news.js
@@ -1,5 +1,4 @@
 // fetch data from category menu
-
 const fetchCategoryData = () => {
   fetch("https://openapi.programming-hero.com/api/news/categories")
     .then((res) => res.json())
@@ -27,7 +26,10 @@ const getCategoryData = (category_id, category_name) => {
   const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => showCategoryNews(data.data, category_name));
+
+    .then((data) => {
+      showCategoryNews(data.data, category_name);
+    });
 };
 
 //show all category News
@@ -68,14 +70,18 @@ const showCategoryNews = (data, category_name) => {
               author.img
             }" class="  rounded-circle" alt="..." height ="50" width = "50">
             <div>
-            <p class="m-0 p-0">${author.name}</p>
+            <p class="m-0 p-0">${
+              author.name ? author.name : "Not Name Available"
+            }</p>
             <p class="m-0 p-0">${author.published_date}</p>
             </div>
             </div>
 
             <div class = "d-flex gap-3">
             <i class="fa-solid fa-eye"></i>
-                <p class ="m-0 p-0">${total_view}</p>
+                <p class ="m-0 p-0">${
+                  total_view ? total_view : "Not Availabe Here"
+                }</p>
             </div>
             <div>
             <i class="fa-solid fa-star"></i>
@@ -101,9 +107,68 @@ const fetchDataDetailsNews = (news_id) => {
   const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => showDataModalFetch(data.data));
+    .then((data) => showDataModalFetch(data.data[0]));
 };
 
 //show on modal when click arrow button
 
-const showDataModalFetch = (newsDetails) => {};
+const showDataModalFetch = (newsDetails) => {
+  console.log(newsDetails);
+  //destrucring object element
+  const {
+    _id,
+    thumbnail_url,
+    title,
+    details,
+    author,
+    total_view,
+    others_info,
+  } = newsDetails;
+
+  document.getElementById("modal-details").innerHTML = `
+          <div class = "card mb-3">
+
+          <div class="row g-0">
+          <div class="col-md-12 px-3 py-3">
+            <img src="${thumbnail_url}" class="img-fluid  mx-auto W-75 rounded-start" alt="...">
+          </div>
+          <div class="col-md-12">
+            <div class="card-body mt-3">
+              <h5 class="card-title ">${title} <span class="badge text-bg-warning">${
+    others_info.is_trending ? "Trending" : "Not Trending"
+  }</span></h5>
+              <p class="card-text">${details}</p>
+
+            </div>
+
+            <div class = " mt-5 card-footer border-o bg-body d-flex justify-content-between">
+            <div class = "d-flex gap-3">
+            <img src="${
+              author.img
+            }" class="  rounded-circle" alt="..." height ="50" width = "50">
+            <div>
+            <p class="m-0 p-0">${
+              author.name ? author.name : "Not Name Available"
+            }</p>
+            <p class="m-0 p-0">${author.published_date}</p>
+            </div>
+            </div>
+
+            <div class = "d-flex gap-3">
+            <i class="fa-solid fa-eye"></i>
+                <p class ="m-0 p-0">${
+                  total_view ? total_view : "Not Availabe Here"
+                }</p>
+            </div>
+            <div>
+            <i class="fa-solid fa-star"></i>
+            </div>
+            
+            </div>
+          </div>
+        </div>
+
+          </div>
+
+          `;
+};
